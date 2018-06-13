2fd123fa7bdab850928308bf38708c53c2d3d43e
kenneth051 <kjdumba@gmail.com>
2018-06-14 02:36:21 +0300

    my web application's UI pages and css

---
+++ UI/index.js
@@ -0,0 +1,24 @@
+function login(){
+	window.location="login.html";
+}
+function register(){
+	window.location="index.html";
+}
+function BacktoHome(){
+	window.location="home.html";
+}
+function availableRides(){
+	window.location="available_rides.html";
+}
+function profile(){
+	window.location="profile.html";
+}
+function requests(){
+	window.location="requests.html";
+}
+function friends(){
+	window.location="friends.html";
+}
+function details(){
+	window.location="rideDetails.html";
+}
