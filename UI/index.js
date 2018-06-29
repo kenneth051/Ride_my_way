dc0e3a59042ddb8b51f87a9e61f08727e78cb06b
kenneth051 <kjdumba@gmail.com>
2018-06-28 19:10:54 -0700

    modfing UI pages in dev

---
+++ UI/index.js
@@ -0,0 +1,40 @@
+function login(){
+	window.location="login.html";
+}
+function register(){
+	window.location="index.html";
+}
+function home(){
+	document.location="home.html";
+}
+function BacktoHome(){
+	window.location="home.html";
+}
+function availableRides(){
+	window.location="available_rides.html";
+}
+function profile(){
+	window.location="profile.html";
+}
+function edit_profile(){
+	window.location="Edit profile.html";
+}
+function requests(){
+	window.location="requests.html";
+}
+function friends(){
+	window.location="friends.html";
+}
+function details(){
+	window.location="rideDetails.html";
+}
+
+function logout(){
+	window.location="index.html";
+}
+function upload(){
+	window.location="upload_picture.html";
+}
+function reminder(){
+	window.location="reminder.html";
+}
